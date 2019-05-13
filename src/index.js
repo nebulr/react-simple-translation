89ba3eeee2d7d5f908c9cf8fde8130889776a6bb
quidestveritas? <quidestveritas@templars.io>
2019-05-12 21:02:37 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable class-methods-use-this */
-import { Component } from 'react';
-import merge from 'deepmerge';
-import uuidv1 from 'uuid/v1';
+import deepmerge from 'deepmerge';
 
-import Store from './store';
-
-export default class StateComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.id = uuidv1();
-    this.state = Store.state;
-    Store.set(this);
+class SimpleTranslations {
+  constructor() {
+    this.config = {
+      language: 'en-US',
+      resources: {},
+    };
   }
 
-  componentWillUnmount() {
-    Store.remove(this.id);
+  init(config) {
+    this.config = deepmerge(this.config, config);
   }
 
-  updateState(obj) {
-    Store.state = merge(Store.state, obj);
-  }
-
-  render() {
-    return (
-      this.props.children
-    );
+  get(key) {
+    return this.config.resources[this.config.language][key];
   }
 }
+
+export default new SimpleTranslations();
